ac286958d9ee73cb41d07d5f32a87851e68dc0c7
Cleber J Santos <cleber@cleberjsantos.com.br>
2019-11-03 20:56:59 -0300

    Initial importing

---
+++ Agenda.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public partial class Agenda : Form
+    {
+        public Agenda()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void SairToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void SobreToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Sobre frm = new Sobre();
+            frm.Show();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# agenda_dot_net
+Agenda
+======
+
